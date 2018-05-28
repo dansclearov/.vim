254eb6d133bf33b09bc3cd86e216c5d9d9d95957
dansclearov <sclearovdan@gmail.com>
2018-05-28 18:29:48 +0300

    added setup.sh for automatic aliasing

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Haven't tested it yet, but it must work
+
+# Vim
+ln -rs .vim/ ~/.vim
+
+# Tmux
+ln -rs .tmux/plugins/ ~/.tmux/plugins
+ln -rs .tmux/tmux.conf ~/.tmux.conf
+
+# Zsh
+ln -rs .zshrc ~/.zshrc
