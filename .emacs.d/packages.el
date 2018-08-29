3f4da9cd26f27dd1d745c6200daf1f8523aea079
dansclearov <sclearovdan@gmail.com>
2018-08-29 10:02:30 +0300

    added more latex snippets to emacs

--- .emacs.d/packages.el
+++ .emacs.d/packages.el
@@ -8,36 +8,46 @@
 (use-package zenburn-theme
   :ensure t
   :defer t)
+
 (use-package gruvbox-theme
   :ensure t
   :defer t)
+
 (use-package markdown-mode
   :ensure t)
+
 (use-package yasnippet
   :ensure t
   :config
-  (yas-global-mode 1))
+  (yas-global-mode 1)
+  (setq yas-triggers-in-field t))
+
 (use-package racket-mode
   :ensure t)
+
 (use-package company
   :ensure t
   :config
   (global-company-mode)
   ;; Add yasnippet to company-mode backends
   (push '(company-semantic :with company-yasnippet) company-backends))
+
 ;; (use-package helm
 ;;   :ensure t
 ;;   :config (helm-mode 1)
 ;;   :bind
 ;;   ("M-x" . helm-M-x)
 ;;   ("C-x C-f" . helm-find-files))
+
 ;; (use-package ivy
 ;;   :ensure t
 ;;   :config (ivy-mode 1))
+
 (use-package haskell-mode
   :ensure t
   :defer t
   :config (setq haskell-process-type 'stack-ghci))
+
 (use-package tex-site
   :ensure auctex
   :config
@@ -48,19 +58,24 @@
         TeX-source-correlate-start-server t)
   ;; Update PDF buffers after successful LaTeX runs
   :hook (TeX-after-compilation-finished-functions . TeX-revert-document-buffer))
+
 (use-package expand-region
   :ensure t
   :bind ("C-=" . er/expand-region))
+
 (use-package ace-window
   :ensure t
   :bind ("M-o" . ace-window))
+
 (use-package pdf-tools
   :ensure t
   :config
   (pdf-tools-install))
+
 ;; (use-package flycheck
 ;;   :ensure t
 ;;   :config (global-flycheck-mode))
+
 (use-package org-journal
   :ensure t
   :defer t
@@ -68,6 +83,7 @@
   (setq org-journal-dir "~/org/journal/")
   (setq org-journal-file-format "%Y-%m-%d")
   (setq org-journal-find-file 'find-file))
+
 ;; (use-package wc-mode
 ;;   :ensure t
 ;;   :init (setq wc-modeline-format "%tw words")
